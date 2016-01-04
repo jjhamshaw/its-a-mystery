1d437b3d83797fd891bfead293c7d2027ab8fe34
Jon Hamshaw <Jonathan_Hamshaw@mckinsey.com>
2016-01-04 17:38:20 +0000

    update

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
 	// Grab the current date
 	var currentDate = new Date();
 
-	// Set some date in the future. In this case, it's always Jan 1
-	var futureDate  = new Date(currentDate.getFullYear() + 1, 0, 1);
+	// 30th jan 2016 10:00am
+	var futureDate  = new Date(2016, 0, 30, 10, 0, 0);
 
 	// Calculate the difference in seconds between the future and current date
 	var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
