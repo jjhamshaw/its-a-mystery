effc50c0daadbb641647d16ea027b1746d852530
Jon Hamshaw <Jonathan_Hamshaw@mckinsey.com>
2016-01-24 17:16:19 +0000

    adding page for when countdown completes

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 var clock;
 
 $(document).ready(function() {
+	$('#next_clue').hide();
 
-	// Grab the current date
 	var currentDate = new Date();
 
-	// 30th jan 2016 10:00am
-	var futureDate  = new Date(2016, 0, 30, 10, 0, 0);
+	// 30th jan 2016 9:30am
+	var futureDate  = new Date(2016, 0, 30, 9, 30, 0);
 
 	// Calculate the difference in seconds between the future and current date
 	var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
@@ -15,6 +15,36 @@ $(document).ready(function() {
 	clock = $('.dw_clock').FlipClock(diff, {
 		clockFace: 'DailyCounter',
 		countdown: true,
-		showSeconds: true
+		showSeconds: true,
+		callbacks: {
+			interval: function(a, b) {
+				init();
+			}
+		}
 	});
+
+	function countdownComplete() {
+		return new Date() > futureDate;
+	}
+
+	function showNumber() {
+		$('#dw_countdown').hide();
+		$('#phone_number').text('07824 358455');
+		$('#next_clue').show();
+	}
+
+	function showCountdown() {
+		$('#dw_countdown').show();
+		$('#next_clue').hide();
+	}
+
+	function init() {
+		if (countdownComplete()) {
+			showNumber();
+		}
+		else {
+			showCountdown();
+		}
+	}
+	init()
 });
